e9a871fd9f69890860d4b47cf94cfc5471c217c0
Surya Saha <ss2489@cornell.edu>
2014-04-30 04:41:36 -0400

    Fixed inverted selection -i flag

--- effector_mirabait/getReads.sh
+++ effector_mirabait/getReads.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
+# Surya Saha
+# Plant Path @Cornell / SOL Genomics
 # Create directories for each effector
 # Get reads that are similar to effector sequences
+
+set -o nounset
+set -o errexit
 
 WDIR=`pwd`
 for EFF in *.fa
@@ -10,8 +14,8 @@ do
 	mkdir "$NAME"; cd "$NAME"
 	cp ../"$EFF" .
 	
-	mirabait -t fastq -k 31 -n 2 -i "$EFF" /scratch/workspace2/data/fwd.paired.9gb-10249472_81PWHABXX_s_6.trim28.cor.quake.fastq fwd.${NAME}.baitk31n2.txt
-	mirabait -t fastq -k 31 -n 2 -i "$EFF" /scratch/workspace2/data/rev.paired.9gb-10249472_81PWHABXX_s_6.trim28.cor.quake.fastq rev.${NAME}.baitk31n2.txt
+	mirabait -t fastq -k 31 -n 2 "$EFF" /scratch/workspace2/data/fwd.paired.9gb-10249472_81PWHABXX_s_6.trim28.cor.quake.fastq fwd.${NAME}.baitk31n2.txt
+	mirabait -t fastq -k 31 -n 2 "$EFF" /scratch/workspace2/data/rev.paired.9gb-10249472_81PWHABXX_s_6.trim28.cor.quake.fastq rev.${NAME}.baitk31n2.txt
 	
 	cd "$WDIR"
 done
